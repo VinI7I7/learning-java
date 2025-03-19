dcc18e6396b1d5d15862f698eec5c213b107a7b4
VinI7I7 <contato.ll.ribeiro@gmail.com>
2025-03-19 11:31:56 -0300

    new package

---
+++ src/learningjava/introducao/TiposPrimitivosExercício03.java
@@ -0,0 +1,21 @@
+package learningjava.introducao;
+/*
+Crie variáveis para os campos descritos abaixo entre <> e imprima a seguinte mensagem:
+
+Eu <nome>, morando no endereço <endereço>,
+confirmo que recebi o salário de <salario>, na data <data>.
+ */
+public class TiposPrimitivosExercício03 {
+    public static void main(String[] args) {
+        String name = "Rebecca";
+        String address = "Street 21";
+        double salary = 1500.00;
+        String salaryReceiptDate = "19/03/2025";
+        String report = "I "+name+ " residing at address "+address+" confirmo que recebi o salário de " +salary+" na data de "+salaryReceiptDate;
+        System.out.print(report);
+
+
+
+
+    }
+}
